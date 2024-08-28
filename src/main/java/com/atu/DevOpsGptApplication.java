968e318a269ea84ec7209138621798e84d2df4f6
Cristiana <cristiana_aso@hotmail.com>
2024-08-28 20:50:43 +0100

    first commit

---
+++ src/main/java/com/atu/DevOpsGptApplication.java
@@ -0,0 +1,14 @@
+package com.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsGptApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DevOpsGptApplication.class, args);
+
+    }
+}
